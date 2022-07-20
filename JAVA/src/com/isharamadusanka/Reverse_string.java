ac5f1f3ab87444c20ed8b3ecfb13465c14b549fb
ChillBroh <isharamadusanka410@gmail.com>
2022-07-21 00:06:24 +0530

    get arr2 size as arr1.length - 1 to fix error

--- JAVA/src/com/isharamadusanka/Reverse_string.java
+++ JAVA/src/com/isharamadusanka/Reverse_string.java
@@ -9,13 +9,14 @@ public class Reverse_string {
         int i, len;
         len = arr.length;
         char[] arr2 = new char[len];
+
         int count = len -1;
         for(i = 0; i < len; i++){
             arr2[count] = arr[i];
             count--;
 
         }
-        for(char x : arr2)
+        for(int x : arr2)
             System.out.print(x);
     }
 }
